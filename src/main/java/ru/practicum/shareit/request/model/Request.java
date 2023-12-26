4a639db32d59e1e1e5293aeb1f0ce99d0544771e
Прокудина Виктория <prokudina.viktoria@yandex.ru>
2023-12-26 09:18:31 +0500

    feat: "фп15"

--- src/main/java/ru/practicum/shareit/request/model/Request.java
+++ src/main/java/ru/practicum/shareit/request/model/Request.java
@@ -24,23 +24,4 @@ public class Request {
     private User requester;
     private LocalDateTime created;
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Request request = (Request) o;
-        return Objects.equals(id, request.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
-
-    @Override
-    public String toString() {
-        return "Request{" +
-                "id=" + id +
-                '}';
-    }
 }
